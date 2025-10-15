16403ddce6b6359816a5be5c6438056beff244a9
LuisHuaranga <luis.huaranga.dev@gmail.com>
2025-10-15 12:10:07 -0500

    Agrege UAT.txt

---
+++ JCLQUERY/02_UAT/b.REVERSE/REVERSE01.sql
@@ -0,0 +1,50 @@
+--
+  SELECT CODMEMB,NUMDOCIDEN,TIPDOCIDEN,NUMSEC FROM D.CDDTPPS1   
+  WHERE CODMEMB IN ('2022241007',                               
+                    '2023000030',                             
+                    '2023000031',                             
+                    '2023050173',                             
+                    '2023050174',                             
+                    '2023050175',                             
+                    '2023050176')                             
+  ORDER BY CODMEMB,NUMDOCIDEN,TIPDOCIDEN,NUMSEC ASC; 
+--
+  UPDATE D.CDDTPPS1
+  SET 
+     NUMDOCIDEN = CASE CODMEMB
+                       WHEN '2022241007' THEN '769251304'
+                       WHEN '2023000030' THEN '91172721'
+                       WHEN '2023000031' THEN '345871692'
+                       WHEN '2023050173' THEN '45105305'
+                       WHEN '2023050174' THEN '20220824'
+                       WHEN '2023050175' THEN '18050199'
+                       WHEN '2023050176' THEN '20220821'
+                       ELSE NUMDOCIDEN
+                    END,
+    TIPDOCIDEN = CASE CODMEMB
+                       WHEN '2022241007' THEN 3
+                       WHEN '2023000030' THEN 1
+                       WHEN '2023000031' THEN 3
+                       WHEN '2023050173' THEN 1
+                       WHEN '2023050174' THEN 3
+                       WHEN '2023050175' THEN 1
+                       WHEN '2023050176' THEN 3
+                       ELSE TIPDOCIDEN
+                    END
+  WHERE CODMEMB IN ('2022241007',
+                    '2023000030',
+                    '2023000031',
+                    '2023050173',
+                    '2023050174',
+                    '2023050175',
+                    '2023050176');
+--
+  SELECT CODMEMB,NUMDOCIDEN,TIPDOCIDEN,NUMSEC FROM D.CDDTPPS1   
+  WHERE CODMEMB IN ('2022241007',                               
+                    '2023000030',                             
+                    '2023000031',                             
+                    '2023050173',                             
+                    '2023050174',                             
+                    '2023050175',                             
+                    '2023050176')                             
+  ORDER BY CODMEMB,NUMDOCIDEN,TIPDOCIDEN,NUMSEC ASC;
